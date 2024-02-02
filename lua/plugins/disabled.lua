b52b02b69dab58950ffc7c982b8913eb335f24d0
54696d654a6f6c74 <eniz.h12@gmail.com>
2024-02-02 10:27:57 +0000

    Remove flash and configure catppucin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "stevearc/dressing.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
