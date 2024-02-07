d2568e0911c5b49a2dc8d046a6c7b9d716c18621
54696d654a6f6c74 <eniz.h12@gmail.com>
2024-02-07 17:27:22 +0000

    Proper support for templ

---
+++ lua/plugins/templ.lua
@@ -0,0 +1,13 @@
+return {
+  "neovim/nvim-lspconfig",
+  html = {
+    setup = {
+      filetypes = { "html", "templ" },
+    },
+  },
+  htmx = {
+    setup = {
+      filetypes = { "html", "templ" },
+    },
+  },
+}
