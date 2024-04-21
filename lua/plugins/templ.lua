ffc2e0e826ff9e65f56d4dfd75463a635feb9a6f
Bitwise <eniz.h12@gmail.com>
2024-04-21 20:55:54 +0100

    Fix templ html compat

--- lua/plugins/templ.lua
+++ lua/plugins/templ.lua
@@ -1,10 +1,5 @@
 return {
   "neovim/nvim-lspconfig",
-  html = {
-    setup = {
-      filetypes = { "html", "templ" },
-    },
-  },
   htmx = {
     setup = {
       filetypes = { "html", "templ" },
