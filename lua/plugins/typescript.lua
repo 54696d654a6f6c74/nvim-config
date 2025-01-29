666f980bfd06e42a4452f2632cbb55a20b86098d
54696d654a6f6c74 <eniz.h12@gmail.com>
2025-01-29 16:51:38 +0000

    Disable ts_ls formatter

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,13 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ts_ls = {
+        settings = {
+          typescript = { format = { enable = false } },
+          javascript = { format = { enable = false } },
+        },
+      },
+    },
+  },
+}
