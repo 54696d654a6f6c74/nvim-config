1b184a60e593deaa9745bf9187a599f0380d3736
Eniz Hasan <enizhasan@Mac.mynet>
2025-11-18 13:02:44 +0000

    improve sourcekit lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,6 @@ return {
         sourcekit = {
           cmd = { "xcrun", "sourcekit-lsp" }, -- adjust if installed elsewhere
           filetypes = { "swift", "objective-c", "objective-cpp" },
-          root_dir = require("lspconfig.util").root_pattern("buildServer.json", "Package.swift", ".git"),
         },
       },
     },
