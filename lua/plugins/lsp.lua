9fe10fac747f39c8d5c0be4a06189db81b5a26d7
Eniz Hasan <enizhasan@Mac.mynet>
2025-11-18 13:02:44 +0000

    improve sourcekit lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,9 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        sourcekit = {
-          cmd = { "xcrun", "sourcekit-lsp" }, -- adjust if installed elsewhere
-          filetypes = { "swift", "objective-c", "objective-cpp" },
-          root_dir = require("lspconfig.util").root_pattern("buildServer.json", "Package.swift", ".git"),
-        },
-      },
-    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.sourcekit.setup({})
+    end,
   },
 }
